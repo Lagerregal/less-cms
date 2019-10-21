3987f86de3d04b6e3850f338c1ab81115388bf50
Markus Hoelzle <mail@markus-hoelzle.de>
2019-10-21 17:35:30 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Development
+
+## Backend
+0. Setup AWS cli
+1. Install npm
+2. `npm install -g serverless`
+3. `npm install`
+4. Deploy API with `sls deploy --stage dev`
+
+## Frontend
+
+`npm install -g @aws-amplify/cli`
+`npm install -g @angular/cli`
+
+`npm install`
+
+Run `ng serve --host=0.0.0.0 --disable-host-check`
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import Amplify, { Auth } from 'aws-amplify';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED - Amazon Cognito Region
+    region: 'eu-central',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'eu-central-1_xoiWy53vC',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '6q5e3b8ss9oegp26eapo2109qn',
+
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: true,
+  }
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure(false);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyAngularModule
+  ],
+  providers: [
+    AmplifyService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
